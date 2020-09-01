3dec84d8c91f748d4eacd7404806f53ed6aec8e5
Slavjan <khaidayev@gmail.com>
2020-09-01 20:18:00 +0700

    repair types.h for compiling

--- types.h
+++ types.h
@@ -1,25 +1,25 @@
 #include <stdint.h>
 
-enum data_val
+typedef enum data_val
 {
 	FAIL,				// Invalid data
 	CONTINUE,			// means that previous action was successfull
 	HALT,				// force stop due to partner's disconnect or another
 	TEST_CONNECT,		// for checking connection
-	TEST_CONNECT_BACK	// answer for previous			
+	TEST_CONNECT_BACK,	// answer for previous			
 	SEND_MAP,  			// next data transfer will be map
 	SHOT,				// client's request for choosen cell
 	HIT,				// shot was successful
 	WET,				// shot wasn't successful
 	WIN,				// you win
-	LOSE,				// you lose
-};
+	LOSE = -1				// you lose
+}data_val_t;
 
 typedef struct event
 {
 	int16_t x;
 	int16_t y;
-	data_val data;
+	data_val_t data;
 } event_t;
 
 char client_ip[16], server_ip[16];
