4305d142ad3c4ef1d68801d00589056dc517d7fc
Slavjan <khaidayev@gmail.com>
2020-09-12 13:07:38 +0700

    server fixed errors

--- server.c
+++ server.c
@@ -14,7 +14,8 @@ int initServer(char *arg)
 	int num_args = 2;
 	char *ip;
 	uint16_t port;
-	while ((opt = getopt(arg, num_args, "a:p:")) != -1) {
+	int opt;
+	while ((opt = getopt(num_args, &arg, "a:p:")) != -1) {
 		switch (opt) 
 		{
 			case 'a':
@@ -25,7 +26,7 @@ int initServer(char *arg)
 				port = atoi(optarg);
                 break;
 			default: /* '?' */
-               fprintf(stderr, "Usage: %s [-a 127.0.0.1] [-p 333] \n", argv[0]);
+               fprintf(stderr, "Usage: initServer [-a 127.0.0.1] [-p 333] \n" );
                exit(EXIT_FAILURE);
 		}
     }	
@@ -35,26 +36,24 @@ int initServer(char *arg)
 	listen( on_serv->local_sock_fd, 2);
 	
 	on_serv->remote_sock_fd = ( int *) malloc( sizeof(int) * 2);
-
+	on_serv->remote_addr = (struct sockaddr_in*) malloc( sizeof( struct sockaddr_in) * 2);
 
 	event_t *game_event = (event_t*) malloc( sizeof(event_t) );
 	
 	status = waitEvent( game_event, on_serv);
 	
 	close( on_serv->local_sock_fd );
-	if( ! on_serv->local_addr )
-		free(on_serv->local_addr);
-	
 	for( int i = 0; i < 1; i++)
 	{
 		close(on_serv->remote_sock_fd[i]);
 
-		if( on_serv->remote_addr[i] )
-		{
-			free(on_serv->remote_addr[i]);
-		}
 	}
-	if( ! on_serv )
+	if( on_serv->remote_addr != NULL )
+	{
+		free(on_serv->remote_addr);
+	}
+
+	if( on_serv != NULL )
 		free(on_serv);
 
 	return status;	
@@ -69,7 +68,6 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 	uint16_t event = ACCEPT_CONNECT;
 	uint16_t users_count = 0;
     user_t *players = (user_t*) malloc( sizeof(user_t) * 2);
-    user_t *token = palyers[0];
 	while( event != NULL )
     {
 	
@@ -80,10 +78,10 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 				if( users_count <= 1 )
                 {
 				    status = acceptConnection(serv_connect->local_sock_fd
-											 , &serv_connect->remote_sock_fd[users_count]
+											 , serv_connect->remote_sock_fd[users_count]
                                              , &serv_connect->remote_addr[users_count]
 											 );
-					players[users_count]->fd = serv_connect->remote_sock_fd[users_count];
+					players[users_count].fd = serv_connect->remote_sock_fd[users_count];
                     event = TEST_CONNECT;
                     users_count++; 
 				}
@@ -95,11 +93,12 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 				if( users_count <= 1 )
 				{
 				    status = sendConnectionTest(serv_connect->remote_sock_fd[users_count]
-											   , serv_connect->remote_addr[users_count]);
+											   , &serv_connect->remote_addr[users_count]);
 					event = ACCEPT_CONNECT;
 				}
 				else{
 					event = WAIT_MAP;
+					e->data = event;
 				}
 			break;
 			case SEND_MAP:  	
@@ -107,13 +106,13 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 				if( users_count >= 0 )
 				{
 					status = sendEvent( serv_connect->remote_sock_fd[users_count]
-									  , e->data = WAIT_MAP
-									  , serv_connect->remote_addr[users_count]);
+									  , e
+									  , &serv_connect->remote_addr[users_count]);
 
 					status = waitMap(serv_connect->remote_sock_fd[users_count]
-									, &players[users_count].map
-									, serv_connect->remote_addr[users_count]);
-					calculateShipPoints( players[users_count] );
+										, (char**) players[users_count].map
+									, &serv_connect->remote_addr[users_count]);
+					calculateShipPoints( &players[users_count] );
 					users_count--;
 					event = WAIT_MAP;
 				}
@@ -126,19 +125,19 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 			case START_GAME:
 				status = sendEvent( serv_connect->remote_sock_fd[users_count]
 									  , e
-									  , serv_connect->remote_addr[users_count]);
+									  , &serv_connect->remote_addr[users_count]);
 			case CONTINUE: 			// means that previous action was successfull
 				if( users_count >= 0 && users_count <= 1 )
 				{
-					if( players[users_count]->hp == 0 ){
+					if( players[users_count].hp == 0 ){
 						event = LOSE;
 						e->data = event;
 						break;
 					}
 
-					status = waitEvent( serv_connect->remote_sock_fd[users_count]
-									  , &e
-									  , serv_connect->remote_addr[users_count]);
+					status = recvEvent( serv_connect->remote_sock_fd[users_count]
+									  , e
+									  , &serv_connect->remote_addr[users_count]);
 					
 					event = e->data;
 				}
@@ -148,7 +147,7 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 			case SHOT:				// client's request for choosen cell
 				if( users_count >= 0 && users_count <= 1 )
 				{
-					event = calculateShot( players[users_count], e->x, e->y);
+					event = calculateShot( &players[users_count], e->x, e->y);
 					e->data = event;
 				}
 			break;
@@ -163,16 +162,16 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 			case HIT:				// shot was successful
 				status = sendEvent( serv_connect->remote_sock_fd[0]
 									, e
-									, serv_connect->remote_addr[0]);
+									, &serv_connect->remote_addr[0]);
 				status = sendEvent( serv_connect->remote_sock_fd[1]
 									, e
-									, serv_connect->remote_addr[1]);
+									, &serv_connect->remote_addr[1]);
 				event = CONTINUE;				
 			break;
 			case LOSE:				// you lose
 				status = sendEvent( serv_connect->remote_sock_fd[users_count]
 									, e
-									, serv_connect->remote_addr[users_count]);
+									, &serv_connect->remote_addr[users_count]);
 				if( users_count == 0 )
 				{
 					users_count = 1;
@@ -185,17 +184,27 @@ int waitEvent(event_t *e, connect_t* serv_connect)
 				e->data = WIN;
 				status = sendEvent( serv_connect->remote_sock_fd[users_count]
 									, e
-									, serv_connect->remote_addr[users_count]);
+									, &serv_connect->remote_addr[users_count]);
 
 			break;
 			case FAIL:
+				if( users_count == 0 )
+				{
+					users_count = 1;
+				}
+				else{
+					users_count = 0;
+				}	
 			case HALT:				// force stop due to partner's disconnect or another
 			default:
-				
+				status = sendEvent( serv_connect->remote_sock_fd[users_count]
+									, e
+									, &serv_connect->remote_addr[users_count]);
 			break;
         }
             
 	}
+	return 0;
 }
 
 
